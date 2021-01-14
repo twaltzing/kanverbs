9d8f048a14cc1df1e02501c3b18d96a5e71fae33
twaltzing <terry.waltz@chineseandspanish.com>
2021-01-14 11:43:30 -0500

    fixed color search

--- js/proBoard.js
+++ js/proBoard.js
@@ -1,8 +1,7 @@
-var stemType = ""; 
- 
+
 var pronounList = [ 
-    [ "a",
-        {"person":"I",
+    [ "a", ["red",
+        [{"person":"I",
         "mohawk":"k",
         "color":"red"} ,
         {"person":"you",
@@ -14,44 +13,368 @@ var pronounList = [
         {"person":"she",
         "mohawk":"yon",
         "color":"red"},
-        
-    ] ,
-    [ "c",
-    {"person":"I",
+        {"person":"it",
+        "mohawk": "wa",
+        "color": "red"},
+        {"person":"we2inc",
+        "mohawk":"tya",
+        "color":"red"},
+        {"person":"youtwo",
+        "mohawk":"tya",
+        "color":"red"},
+        {"person":"2M",
+        "mohawk":"ya",
+        "color": "red"},
+        {"person":"2F",
+        "mohawk":"kya",
+        "color":"red"},
+        {"person":"we3inc",
+        "mohawk":"tewa",
+        "color":"red"},
+        {"person":"we3exc",
+        "mohawk":"yakwa",
+        "color":"red"},
+        {"person":"you3",
+        "mohawk":"sewa",
+        "color": "red"},
+        {"person":"3M",
+        "mohawk":"ron",
+        "color":"red"},
+        {"person":"3F",
+        "mohawk":"kon",
+        "color":"red"}]],
+
+        ["blue", 
+             [{"person":"I",
+            "mohawk":"wak",
+            "color":"blue"},
+            {"person":"you",
+            "mohawk":"sa",
+            "color":"blue"},
+            {"person":"he",
+            "mohawk":"ro",
+            "color":"blue"},
+            {"person":"she",
+            "mohawk":"yako",
+            "color":"blue"},
+            {"person":"it",
+            "mohawk":"yo",
+            "color":"blue"},
+            {"person":"we2inc",
+            "mohawk":"yonky",
+            "color":"blue"},
+            {"person":"we2exc",
+            "mohawk":"yonky",
+            "color":"blue"},
+            {"person":"youtwo",
+            "mohawk":"tsy",
+            "color":"blue"},
+            {"person":"2M",
+            "mohawk":"ron",
+            "color":"blue"},
+            {"person":"2F",
+            "mohawk":"yon",
+            "color":"blue"},
+            {"person":"we3inc",
+            "mohawk":"yonkw",
+            "color":"blue"},
+            {"person":"you3",
+            "mohawk":"sew",
+            "color":"blue"},
+            {"person":"we3exc",
+            "mohawk":"yonkw",
+            "color":"blue"},
+            {"person":"3M",
+            "mohawk":"ron",
+            "color":"blue"},
+            {"person":"3F",
+            "mohawk":"yon",
+            "color":"blue"}
+        ]]] ,
+        [ "c", ["red", 
+    [{"person":"I",
     "mohawk":"ke",
     "color":"red"} ,
-    
     {"person":"you",
     "mohawk":"se",
     "color":"red"},
-
     {"person":"he",
-        "mohawk":"ra",
-        "color":"red"},
-
-        {"person":"she",
-        "mohawk":"ye",
-        "color":"red"}
-]   
+    "mohawk":"ra",
+    "color":"red"},
+    {"person":"she",
+    "mohawk":"ye",
+    "color":"red"},
+    {"person":"it",
+    "mohawk":"ka",
+    "color":"red"},
+    {"person":"we2inc",
+    "mohawk":"teni",
+    "color":"red"},
+    {"person":"we2exc",
+    "mohawk":"yakeni",
+    "color":"red"},
+    {"person":"youtwo",
+    "mohawk":"seni",
+    "color":"red"},
+    {"person":"2M",
+    "mohawk":"ni",
+    "color":"red"},
+    {"person":"2F",
+    "mohawk":"keni",
+    "color":"red"},
+    {"person":"we3inc",
+    "mohawk":"tewa",
+    "color":"red"},
+    {"person":"we3exc",
+    "mohawk":"yakwa",
+    "color":"red"},
+    {"person":"you3",
+    "mohawk":"sewa",
+    "color":"red"},
+    {"person":"3M",
+    "mohawk":"roti",
+    "color":"red"},
+    {"person":"3F",
+    "mohawk":"konti",
+    "color":"red"
+    }]],
+    [["blue",
+    {"person":"I",
+ "mohawk":"wake",
+ "color":"blue"},
+ {"person":"you",
+ "mohawk":"sa",
+ "color":"blue"},
+ {"person":"he",
+ "mohawk":"ro",
+ "color":"blue"},
+ {"person":"she",
+ "mohawk":"yako",
+ "color":"blue"},
+ {"person":"it",
+ "mohawk":"yo",
+ "color":"blue"},
+ {"person":"we2inc",
+ "mohawk":"yonkeni",
+ "color":"blue"},
+ {"person":"we2exc",
+ "mohawk":"yonkeni",
+ "color":"blue"},
+ {"person":"youtwo",
+ "mohawk":"seni",
+ "color":"blue"},
+ {"person":"2M",
+ "mohawk":"roti",
+ "color":"blue"},
+ {"person":"2F",
+ "mohawk":"yoti",
+ "color":"blue"},
+ {"person":"we3inc",
+ "mohawk":"yonkwa",
+ "color":"blue"},
+ {"person":"you3",
+ "mohawk":"sewa",
+ "color":"blue"},
+ {"person":"we3exc",
+ "mohawk":"yonkwa",
+ "color":"blue"},
+ {"person":"3M",
+ "mohawk":"roti",
+ "color":"blue"},
+ {"person":"3F",
+ "mohawk":"yoti",
+ "color":"blue"} 
+ ]]],
+    ["e",["red",
+    [{"person":"I",
+    "mohawk":"k",
+    "color":"red"} ,
+    {"person":"you",
+    "mohawk":"s",
+    "color":"red"},
+    {"person":"he",
+    "mohawk":"r",
+    "color":"red"},
+    {"person":"she",
+    "mohawk":"yak",
+    "color":"red"},
+    {"person":"it",
+    "mohawk":"w",
+    "color":"red"},
+    {"person":"we2inc",
+    "mohawk":"ten",
+    "color":"red"},
+    {"person":"we2exc",
+    "mohawk":"yaken",
+    "color":"red"},
+    {"person":"youtwo",
+    "mohawk":"sen",
+    "color":"red"},
+    {"person":"2M",
+    "mohawk":"n",
+    "color":"red"},
+    {"person":"2F",
+    "mohawk":"ken",
+    "color":"red"},
+    {"person":"we3inc",
+    "mohawk":"tew",
+    "color":"red"},
+    {"person":"we3exc",
+    "mohawk":"yakw",
+    "color":"red"},
+    {"person":"you3",
+    "mohawk":"sew",
+    "color":"red"},
+    {"person":"3M",
+    "mohawk":"ronn",
+    "color":"red"},
+    {"person":"3F",
+    "mohawk":"konn",
+    "color":"red"
+    }    
+    ]]],
+ 
+ [
+    "i",["red",
+    [{"person":"I",
+    "mohawk":"k",
+    "color":"red"} ,
+    {"person":"you",
+    "mohawk":"s",
+    "color":"red"},
+    {"person":"he",
+    "mohawk":"ren",
+    "color":"red"},
+    {"person":"she",
+    "mohawk":"ye",
+    "color":"red"},
+    {"person":"it",
+    "mohawk":"ken",
+    "color":"red"},
+    {"person":"we2inc",
+    "mohawk":"ten",
+    "color":"red"},
+    {"person":"we2exc",
+    "mohawk":"yaken",
+    "color":"red"},
+    {"person":"youtwo",
+    "mohawk":"sen",
+    "color":"red"},
+    {"person":"2M",
+    "mohawk":"n",
+    "color":"red"},
+    {"person":"2F",
+    "mohawk":"ken",
+    "color":"red"},
+    {"person":"we3inc",
+    "mohawk":"tewen",
+    "color":"red"},
+    {"person":"we3exc",
+    "mohawk":"yakwen",
+    "color":"red"},
+    {"person":"you3",
+    "mohawk":"sewen",
+    "color":"red"},
+    {"person":"3M",
+    "mohawk":"rat",
+    "color":"red"},
+    {"person":"3F",
+    "mohawk":"kont",
+    "color":"red"
+    }
+    ]]],
+    
+    ["q",["green",
+    [{"person":"someone",
+    "mohawk": "hati óhnka"}]]]
 ]
+
 
 var verbRoots = [
     {"english":"likes",
-    "mohawk":"nònwe&#39;s"},
+    "mohawk":"nònwe\'s",
+    "color":"red"},
     {"english":"remembers",
-    "mohawk":"ehyá:ra&#39;s"},
+    "mohawk":"ehyá:ra\'s",
+    "color":"red"},
     {"english":"looks for",
-    "mohawk": "é:sak&#39;s"},
+    "mohawk": "é:sak\'s",
+    "color":"red"},
     {"english":"stays at",
-    "mohawk": "í:teron"},
+    "mohawk": "í:teron",
+    "color":"red"},
     {"english":"sews",
-    "mohawk":"&#39;níkhons"}
+    "mohawk":"′níkhons",
+    "color":"red"},
+    {"english":"understands",
+    "mohawk": "′nikonhrayén:ta\'s",
+    "color":"blue"},
+    {"english":"being sick",
+    "mohawk": "nonhwáktani",
+    "color":"blue"},
+    {"english": "resting",
+    "mohawk": "atoríhsen",
+    "color":"blue"},
+    {"english":"try hard",
+    "mohawk":"ahkwíhsron",
+    "color":"red"}
+
 ]
 
+
+const pronounTest = [
+    {
+    stemColor: "red",
+    stemLetter: "a",
+    pronouns:
+    {   I: "k",
+            you: 's',
+            he: "r",
+            she: "yon",
+            it: "w"}
+             
+    },
+    {
+        stemColor: "blue",
+        stemLetter: "a",
+        pronouns: {
+                I: "wak",
+                you: 'blues',
+                he: "bluer",
+                she: "yak",
+                it: "bluew"
+            }},
+
+            {
+                stemColor: "red",
+                stemLetter: "c",
+                pronouns: {
+                        I: "hello",
+                        you: 'blues',
+                        he: "bluer",
+                        she: "yak",
+                        it: "bluew"
+                    }}
+]
+
+for (i=0; i<pronounTest.length; i++){
+    console.log(pronounTest[i].stemLetter);
+if (pronounTest[i].color==="blue" && pronounTest[i].stemLetter==="c")
+{
+console.log(pronounTest[i].pronouns.I);
+}}
+
+
+
+
 var buttonStuff="";
+var stemType = ""; 
+var stem="";
+var stemColor="";
 
 verbRoots.forEach((verb)=>{
-    buttonStuff += `<button class="verbGenerator" data-mohawk=${verb.mohawk}>${verb.english}</button>`;
+    buttonStuff += `<button class="verbGenerator" data-mohawk=${verb.mohawk} data-color=${verb.color}>${verb.english}</button>`;
+    
 });
 
 
@@ -95,25 +418,56 @@ function showPro(person) {
     var x = document.getElementById("prefixes");
     x.innerHTML="";
     //get root type
-
     stemLetter=document.getElementById("stem").value[0];
-
+    //group all consonants as c stems
     var consonant=isConsonant(stemLetter);
     console.log("StemLetter is now "+stemLetter);
- 
-    //match corresponding pronoun by person
-for (i=0; i<pronounList.length; i++){
-    // match stem
-    if (pronounList[i][0]===stemLetter) {
-        for (j=0; j<pronounList[i].length; j++){
-            if (pronounList[i][j].person === person) {
-                x.innerHTML=pronounList[i][j].mohawk;
-            }
-        }
+
+    //get the stem we're working with
+    stem=document.getElementById("stem").value;
+    console.log("Stem is "+stem);
+
+/*     console.log(verbRoots[1].color);
+    stemColor=verbRoots[1].color;
+    console.log(stemColor+" is the color"); */
+
+    for (r=0; r<verbRoots.length; r++) {
+        /* console.log("Root "+r+" is "+verbRoots[r].mohawk); */
+        //find the stem color
+        if (verbRoots[r].mohawk===stem){
+            console.log("****THIS ONE!****");
+            stemColor=verbRoots[r].color;
+            
         }
     }
-}
+   console.log("Stem color is "+stemColor); 
 
+    //match corresponding pronoun by person
+    for (i=0; i<pronounTest.length; i++){
+        console.log("StemLetter of data is "+pronounTest[i].stemLetter); 
+        console.log("StemColor of data is "+pronounTest[i].stemColor);
+        console.log("We seek "+ stemLetter ,stemColor);
+    if (pronounTest[i].stemLetter === stemLetter && pronounTest[i].stemColor === stemColor)
+    {
+        console.log("Found letter and color");
+        /* 0 */
+
+        let pickList = pronounTest[i].pronouns;
+        var item;
+        if( pickList.hasOwnProperty(person) ) {
+            item = pickList[person];
+            console.log("You are looking for "+ item );
+
+            x.innerHTML=item;
+        }
+
+
+
+}
+    
+        }
+
+    }
 
 
 function showPersonButtons(stem){
